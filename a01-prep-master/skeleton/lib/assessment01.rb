578cba9f16216914d41aaea327cc1d8d5c6bc48e
App Academy Student <appacademy@Apps-Mac-mini-63.local>
2016-04-17 18:23:39 -0400

    9 failures out of 25 examples.

--- a01-prep-master/skeleton/lib/assessment01.rb
+++ a01-prep-master/skeleton/lib/assessment01.rb
@@ -1,14 +1,40 @@
+require 'byebug'
+
 class Array
   def my_inject(accumulator = nil, &prc)
+    # prc ||= Proc.new {|accum, el| }
+    # default Proc
+    skip_one = false
+    if accumulator.nil?
+      accumulator = self.first
+      skip_one = true
+    end
+    self.each do |el|
+      if skip_one
+        skip_one = false
+        next
+      end
+      accumulator = prc.call(accumulator, el)
+    end
+    accumulator
+  end
 
 end
 
 def is_prime?(num)
-
+  num > 1 ? (2..num-1).all? {|x| num % x != 0} : false
 end
 
 def primes(count)
-
+  return [] if count == 0 # DON'T MESS UP = WITH ==
+  primes = []
+  checking = 2
+  until primes.length == count
+    # debugger if count > 0
+    primes << checking if is_prime?(checking)
+    checking += 1
+  end
+  primes
 end
 
 # the "calls itself recursively" spec may say that there is no method
@@ -18,7 +44,14 @@ end
 # to equal 1. So the 2nd factorial is 1!, the 3rd factorial is 2!, etc.
 
 def factorials_rec(num)
-
+  return [1] if num == 1
+  return [1,1] if num == 2
+  facts = []
+  until facts.length == num
+    facts = factorials_rec(num - 1)
+    facts << facts.last * (num - 1)
+  end
+  facts
 end
 
 class Array
@@ -27,13 +60,26 @@ end
 
 class String
 
-  def substrings(string)
-
+  def substrings
+    start = 0
+    subs = []
+    while start < self.length - 1
+      stop = start + 1
+      while stop < self.length
+        subs << self[start..stop]
+        stop += 1
+      end
+      start += 1
+    end
+    subs
   end
 
 
   def symmetric_substrings
+    subs = self.substrings
+    subs.select {|x| x == x.reverse}
 
+  end
 end
 
 class Array
@@ -43,6 +89,6 @@ class Array
 
   private
   def self.merge(left, right, &prc)
-  
+
   end
 end
